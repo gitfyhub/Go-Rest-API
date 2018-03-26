b3f40416775c31815c8f4ab19ffc7b25c63ee21a
Faisal Younus <gitfyhub>
2018-03-25 23:03:57 -0500

    initial commit

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// Get Single Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	println("GetBook")
+}
+
+// Create Book
+func createBook(w http.ResponseWriter, r *http.Request) {
+}
+
+// Update Book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+}
+
+// Delete book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+}
+
+// Book Struct (Model)
+type Book struct {
+	ID     string  `json:"id"`
+	Isbn   string  `json:"isbn"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// Author Struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+// Init books var as a slice book struct
+var books []Book
+
+func main() {
+	// Init Router
+	r := mux.NewRouter()
+
+	// Mock Data
+	books = append(books, Book{ID: "1", Isbn: "448743", Title: "Somebook", Author: &Author{Firstname: "Saleem", Lastname: "Baba"}})
+	books = append(books, Book{ID: "2", Isbn: "443459", Title: "Somebook II", Author: &Author{Firstname: "Yaseen", Lastname: "Baba"}})
+
+	// Route Handlers / Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/book/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
